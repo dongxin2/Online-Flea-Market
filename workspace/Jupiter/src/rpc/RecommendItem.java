5b1ccb5e2796e576ea32346b7a58fb89310a3b5b
dongxin zhu <dongxinzhu@dongxins-MacBook-Pro.local>
2019-03-23 19:56:35 -0500

    add my workspace

---
+++ workspace/Jupiter/src/rpc/RecommendItem.java
@@ -0,0 +1,55 @@
+package rpc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Servlet implementation class RecommendItem
+ */
+@WebServlet("/recommendation")
+public class RecommendItem extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RecommendItem() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		JSONArray array = new JSONArray();
+		try {
+			array.put( new JSONObject().put("name", "John")
+					.put("desk", "20").put("computer", "500") );
+			array.put( new JSONObject().put("name", "Alice")
+					.put("vacuum", "20").put("doll", "5"));
+		} catch(JSONException e) {
+			e.printStackTrace();
+		}
+		RpcHelper.writeJsonArray(response, array);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
